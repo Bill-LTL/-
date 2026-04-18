60b9f9a0ac030cf23c4753a6626e4429b92b25ab
Bill <ltl.bill.ltl.fcb@gmail.com>
2026-04-18 17:38:54 +0800

    優化代碼

--- somefunc.h
+++ somefunc.h
@@ -64,15 +64,6 @@ void auto_fill(char *key, char *val) {
             strcpy(target_address, val);   
         }
     }
-    //優先自定義獲取ipv6命令
-    if(strcmp(cfg.cmd_get6, "default") == 0){
-        getipv6 = "ip -6 addr show enp6s0 | grep 'scope global' | grep '/128' | awk '{print $2}' | cut -d'/' -f1";
-        printf("%s 使用預設獲取IPv6命令: %s\n", get_now(), GET_IPV6);  
-            
-    }else{
-        getipv6 = cfg.cmd_get6;
-        printf("%s 使用配置文件獲取IPv6命令: %s\n", get_now(), getipv6);  
-    }
     return;
 }
 
